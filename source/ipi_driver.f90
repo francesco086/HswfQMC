a9047925188d6006c14a985ac0cca0cbd7029f5b
Jan Kessler <jakessle@t-online.de>
2015-07-26 20:41:59 +0200

    Corrections to make last version compilable

--- source/ipi_driver.f90
+++ source/ipi_driver.f90
@@ -29,155 +29,156 @@
 ! -------------------------------------------------------------------------
 ! Modified by Jan Kessler (2015)
 ! An adaption of the I-Pi driver example to HswfQMC
-    PROGRAM IPI_DRIVER
+PROGRAM IPI_DRIVER
 
-      IMPLICIT NONE
+    IMPLICIT NONE
 
-      ! SOCKET VARIABLES
-      INTEGER, PARAMETER :: MSGLEN=12   ! length of the headers of the driver/wrapper communication protocol
-      INTEGER socket, inet, port        ! socket ID & address of the server
-      CHARACTER*1024 :: host
+    ! SOCKET VARIABLES
+    INTEGER, PARAMETER :: MSGLEN=12   ! length of the headers of the driver/wrapper communication protocol
+    INTEGER socket, inet, port        ! socket ID & address of the server
+    CHARACTER*1024 :: host
 
-      ! COMMAND LINE PARSING
-      CHARACTER*1024 :: cmdbuffer, vops
-      INTEGER ccmd, vstyle
-      LOGICAL verbose
-      INTEGER commas(4), par_count      ! stores the index of commas in the parameter string
-      INTEGER vpars(4)                  ! array to store the parameters of the potential
+    ! COMMAND LINE PARSING
+    CHARACTER*1024 :: cmdbuffer, vops
+    INTEGER ccmd, vstyle
+    LOGICAL verbose
+    INTEGER commas(4), par_count      ! stores the index of commas in the parameter string
+    INTEGER vpars(4)                  ! array to store the parameters of the potential
 
-      ! SOCKET COMMUNICATION BUFFERS
-      CHARACTER*12 :: header
-      LOGICAL :: isinit=.false., hasdata=.false.
-      INTEGER cbuf
-      CHARACTER*2048 :: initbuffer      ! it's unlikely a string this large will ever be passed...
-      DOUBLE PRECISION, ALLOCATABLE :: msgbuffer(:)
+    ! SOCKET COMMUNICATION BUFFERS
+    CHARACTER*12 :: header
+    LOGICAL :: isinit=.false., hasdata=.false.
+    INTEGER cbuf
+    CHARACTER*2048 :: initbuffer      ! it's unlikely a string this large will ever be passed...
+    DOUBLE PRECISION, ALLOCATABLE :: msgbuffer(:)
 
-      ! PARAMETERS OF THE SYSTEM (CELL, ATOM POSITIONS, ...)
-      DOUBLE PRECISION              :: box(3), pot
-      DOUBLE PRECISION, ALLOCATABLE :: atoms(:,:), forces(:,:)
-      INTEGER nat
-      DOUBLE PRECISION cell_h(3,3), cell_ih(3,3), virial(3,3)
+    ! PARAMETERS OF THE SYSTEM (CELL, ATOM POSITIONS, ...)
+    DOUBLE PRECISION              :: box(3), pot
+    DOUBLE PRECISION, ALLOCATABLE :: atoms(:,:), forces(:,:)
+    INTEGER nat
+    DOUBLE PRECISION cell_h(3,3), cell_ih(3,3), virial(3,3)
 
-      ! PARAMETERS CONCERNING VMC (NCPUS,N_WOMIN)
-      INTEGER ncpus,n_womin
+    ! PARAMETERS CONCERNING VMC (NCPUS,NWOMIN)
+    INTEGER ncpus,nwomin
 
 
-      INTEGER i
+    INTEGER i,k,l
 
-      ! parse the command line parameters
-      ! intialize defaults
-      ccmd = 0
-      inet = 1
-      host = "localhost"//achar(0)
-      port = 31415
-      verbose = .false.
-      par_count = 0
-      vstyle = -1
+    ! parse the command line parameters
+    ! intialize defaults
+    ccmd = 0
+    inet = 1
+    host = "localhost"//achar(0)
+    port = 31415
+    verbose = .false.
+    par_count = 0
+    vstyle = -1
 
-      DO i = 1, IARGC()
-         CALL GETARG(i, cmdbuffer)
-         IF (cmdbuffer == "-u") THEN ! flag for unix socket
+    DO i = 1, IARGC()
+        CALL GETARG(i, cmdbuffer)
+        IF (cmdbuffer == "-u") THEN ! flag for unix socket
             inet = 0
             ccmd = 0
-         ELSEIF (cmdbuffer == "-h") THEN ! read the hostname
+        ELSEIF (cmdbuffer == "-h") THEN ! read the hostname
             ccmd = 1
-         ELSEIF (cmdbuffer == "-p") THEN ! reads the port number
+        ELSEIF (cmdbuffer == "-p") THEN ! reads the port number
             ccmd = 2
-         ELSEIF (cmdbuffer == "-m") THEN ! reads the handling of electronic parameter optimization
+        ELSEIF (cmdbuffer == "-m") THEN ! reads the handling of electronic parameter optimization
             ccmd = 3
-         ELSEIF (cmdbuffer == "-o") THEN ! reads the parameters
+        ELSEIF (cmdbuffer == "-o") THEN ! reads the parameters
             ccmd = 4
-         ELSEIF (cmdbuffer == "-v") THEN ! flag for verbose standard output
+        ELSEIF (cmdbuffer == "-v") THEN ! flag for verbose standard output
             verbose = .true.
-         ELSE
+        ELSE
             IF (ccmd == 0) THEN
-               WRITE(*,*) " Unrecognized command line argument", ccmd
-               WRITE(*,*) " SYNTAX: ipi_driver.x [-u] -h hostname -p port -m [alt|sim] -o 'comma_separated_parameters' [-v] "
-               WRITE(*,*) ""
-               WRITE(*,*) " For the alternating mode (alt), use -o NCPU,N_WOMIN (steps without new energy minimum in parameter optimization"
-               WRITE(*,*) " For the simultaneous mode (sim), use -o NCPU! "
-               CALL EXIT(-1)
+                WRITE(*,*) " Unrecognized command line argument", ccmd
+                WRITE(*,*) " SYNTAX: ipi_driver.x [-u] -h hostname -p port -m [alt|sim] -o 'comma_separated_parameters' [-v] "
+                WRITE(*,*) ""
+                WRITE(*,*) " For the alternating mode (alt), use -o NCPU,NWOMIN &
+               (steps without new energy minimum in parameter optimization"                
+                WRITE(*,*) " For the simultaneous mode (sim), use -o NCPU! "
+                CALL EXIT(-1)
             ENDIF
             IF (ccmd == 1) THEN
-               host = trim(cmdbuffer)//achar(0)
+                host = trim(cmdbuffer)//achar(0)
             ELSEIF (ccmd == 2) THEN
-               READ(cmdbuffer,*) port
+                READ(cmdbuffer,*) port
             ELSEIF (ccmd == 3) THEN
-               IF (trim(cmdbuffer) == "alt") THEN
-                  vstyle = 1
-               ELSEIF (trim(cmdbuffer) == "sim") THEN
-                  vstyle = 0
-               ELSE
-                  WRITE(*,*) " Unrecognized optimization mode ", trim(cmdbuffer)
-                  WRITE(*,*) " Use -m [alt|sim] "
-                  CALL EXIT(-1)
-               ENDIF
+                IF (trim(cmdbuffer) == "alt") THEN
+                    vstyle = 1
+                ELSEIF (trim(cmdbuffer) == "sim") THEN
+                    vstyle = 0
+                ELSE
+                    WRITE(*,*) " Unrecognized optimization mode ", trim(cmdbuffer)
+                    WRITE(*,*) " Use -m [alt|sim] "
+                    CALL EXIT(-1)
+                ENDIF
             ELSEIF (ccmd == 4) THEN
-               par_count = 1
-               commas(1) = 0
-               DO WHILE (index(cmdbuffer(commas(par_count)+1:), ',') > 0) 
-                  commas(par_count + 1) = index(cmdbuffer(commas(par_count)+1:), ',') + commas(par_count)
-                  READ(cmdbuffer(commas(par_count)+1:commas(par_count + 1)-1),*) vpars(par_count)
-                  par_count = par_count + 1
-               ENDDO
-               READ(cmdbuffer(commas(par_count)+1:),*) vpars(par_count)
+                par_count = 1
+                commas(1) = 0
+                DO WHILE (index(cmdbuffer(commas(par_count)+1:), ',') > 0)
+                    commas(par_count + 1) = index(cmdbuffer(commas(par_count)+1:), ',') + commas(par_count)
+                    READ(cmdbuffer(commas(par_count)+1:commas(par_count + 1)-1),*) vpars(par_count)
+                    par_count = par_count + 1
+                ENDDO
+                READ(cmdbuffer(commas(par_count)+1:),*) vpars(par_count)
             ENDIF
             ccmd = 0
-         ENDIF
-      ENDDO
+        ENDIF
+    ENDDO
 
-      IF (vstyle == 0) THEN
-         IF (par_count /= 1) THEN
+    IF (vstyle == 0) THEN
+        IF (par_count /= 1) THEN
             WRITE(*,*) "Error: parameters not initialized correctly."
-            WRITE(*,*) "For alt mode use -o N_WOMIN "
+            WRITE(*,*) "For alt mode use -o NWOMIN "
             CALL EXIT(-1) ! Note that if initialization from the wrapper is implemented this exit should be removed.
-         ENDIF
-         ncpus = vpars(1)
-         isinit = .true.
-      ELSEIF (vstyle == 1) THEN
-         IF (par_count /= 2) THEN
+        ENDIF
+        ncpus = vpars(1)
+        isinit = .true.
+    ELSEIF (vstyle == 1) THEN
+        IF (par_count /= 2) THEN
             WRITE(*,*) "Error: parameters not initialized correctly."
-            WRITE(*,*) "For alt mode use -o N_WOMIN "
+            WRITE(*,*) "For alt mode use -o NWOMIN "
             CALL EXIT(-1) ! Note that if initialization from the wrapper is implemented this exit should be removed.
-         ENDIF
-         ncpus = vpars(1)
-         nwomin = vpars(2)
-         isinit = .true.
-      ENDIF
+        ENDIF
+        ncpus = vpars(1)
+        nwomin = vpars(2)
+        isinit = .true.
+    ENDIF
 
-      IF (verbose) THEN
-         WRITE(*,*) " DRIVER - Connecting to host ", trim(host)
-         IF (inet > 0) THEN
+    IF (verbose) THEN
+        WRITE(*,*) " DRIVER - Connecting to host ", trim(host)
+        IF (inet > 0) THEN
             WRITE(*,*) " on port ", port, " using an internet socket."
-         ELSE
+        ELSE
             WRITE(*,*) " using an UNIX socket."
-         ENDIF
-      ENDIF
+        ENDIF
+    ENDIF
 
-      ! Calls the interface to the C sockets to open a communication channel
-      CALL open_socket(socket, inet, port, host)
-      nat = -1
-      DO WHILE (.true.) ! Loops forever (or until the wrapper ends!)
+    ! Calls the interface to the C sockets to open a communication channel
+    CALL open_socket(socket, inet, port, host)
+    nat = -1
+    DO WHILE (.true.) ! Loops forever (or until the wrapper ends!)
 
-         ! Reads from the socket one message header
-         CALL readbuffer(socket, header, MSGLEN)
-         IF (verbose) WRITE(*,*) " Message from server: ", trim(header)
+        ! Reads from the socket one message header
+        CALL readbuffer(socket, header, MSGLEN)
+        IF (verbose) WRITE(*,*) " Message from server: ", trim(header)
 
-         IF (trim(header) == "STATUS") THEN
+        IF (trim(header) == "STATUS") THEN
             ! The wrapper is inquiring on what we are doing
             IF (.not. isinit) THEN
-               CALL writebuffer(socket,"NEEDINIT    ",MSGLEN)  ! Signals that we need initialization data
+                CALL writebuffer(socket,"NEEDINIT    ",MSGLEN)  ! Signals that we need initialization data
             ELSEIF (hasdata) THEN
-               CALL writebuffer(socket,"HAVEDATA    ",MSGLEN)  ! Signals that we are done computing and can return forces
+                CALL writebuffer(socket,"HAVEDATA    ",MSGLEN)  ! Signals that we are done computing and can return forces
             ELSE
-               CALL writebuffer(socket,"READY       ",MSGLEN)  ! We are idling and eager to compute something
+                CALL writebuffer(socket,"READY       ",MSGLEN)  ! We are idling and eager to compute something
             ENDIF
-         ELSEIF (trim(header) == "INIT") THEN     ! The driver is kindly providing a string for initialization
+        ELSEIF (trim(header) == "INIT") THEN     ! The driver is kindly providing a string for initialization
             CALL readbuffer(socket, cbuf, 4)
             CALL readbuffer(socket, initbuffer, cbuf)
             IF (verbose) WRITE(*,*) " Initializing system from wrapper, using ", trim(initbuffer)
             isinit=.true. ! We actually do nothing with this string, thanks anyway. Could be used to pass some information (e.g. the input parameters, or the index of the replica, from the driver
-         ELSEIF (trim(header) == "POSDATA") THEN  ! The driver is sending the positions of the atoms. Here is where we do the calculation!
+        ELSEIF (trim(header) == "POSDATA") THEN  ! The driver is sending the positions of the atoms. Here is where we do the calculation!
 
             ! Parses the flow of data from the socket
             CALL readbuffer(socket, cell_h,  9*8)  ! Cell matrix
@@ -195,21 +196,21 @@
 
             CALL readbuffer(socket, cbuf, 4)       ! The number of atoms in the cell
             IF (nat < 0) THEN  ! Assumes that the number of atoms does not change throughout a simulation, so only does this once
-               nat = cbuf
-               IF (verbose) WRITE(*,*) " Allocating buffer and data arrays, with ", nat, " atoms"
-               ALLOCATE(msgbuffer(3*nat))
-               ALLOCATE(atoms(nat,3))
-               ALLOCATE(forces(nat,3))
+                nat = cbuf
+                IF (verbose) WRITE(*,*) " Allocating buffer and data arrays, with ", nat, " atoms"
+                ALLOCATE(msgbuffer(3*nat))
+                ALLOCATE(atoms(nat,3))
+                ALLOCATE(forces(nat,3))
             ENDIF
 
             CALL readbuffer(socket, msgbuffer, nat*3*8)
             DO i = 1, nat
-               atoms(i,:) = msgbuffer(3*(i-1)+1:3*i)
+                atoms(i,:) = msgbuffer(3*(i-1)+1:3*i)
             ENDDO
             OPEN (UNIT=20, FILE='rp_now.d', STATUS='REPLACE', ACTION='WRITE')
             write(20,*) box
             DO i = 1, nat
-               write(20,*) atoms(i,:)
+                write(20,*) atoms(i,:)
             ENDDO
             CLOSE(20)
 
@@ -223,26 +224,26 @@
                 virial = 0
             END IF
 
-            pot = 0.5*ks*atoms(1,1)**2
+            pot = 0.5*atoms(1,1)**2
             forces = 0
-            forces(1,1) = -ks*atoms(1,1)
+            forces(1,1) = -atoms(1,1)
             virial = 0
             DO i = 1, nat
-               DO k = 1, 3
-                   DO l = k, 3
-                  ! Only the upper triangular elements calculated.
-                       virial(k,l) = virial(k,l) + forces(i,k)*atoms(i,l)
-                   ENDDO
-               ENDDO
+                DO k = 1, 3
+                    DO l = k, 3
+                        ! Only the upper triangular elements calculated.
+                        virial(k,l) = virial(k,l) + forces(i,k)*atoms(i,l)
+                    ENDDO
+                ENDDO
             ENDDO
             IF (verbose) WRITE(*,*) " Calculated energy is ", pot
-            ENDIF
+
             hasdata = .true. ! Signal that we have data ready to be passed back to the wrapper
-         ELSEIF (trim(header) == "GETFORCE") THEN  ! The driver calculation is finished, it's time to send the results back to the wrapper
+        ELSEIF (trim(header) == "GETFORCE") THEN  ! The driver calculation is finished, it's time to send the results back to the wrapper
 
             ! Data must be re-formatted (and units converted) in the units and shapes used in the wrapper
             DO i = 1, nat
-               msgbuffer(3*(i-1)+1:3*i) = forces(i,:)
+                msgbuffer(3*(i-1)+1:3*i) = forces(i,:)
             ENDDO
             virial = transpose(virial)
 
@@ -256,10 +257,10 @@
             CALL writebuffer(socket,"nothing",7)
 
             hasdata = .false.
-         ELSE
+        ELSE
             WRITE(*,*) " Unexpected header ", header
             CALL EXIT(-1)
-         ENDIF
-      ENDDO
-      IF (nat > 0) DEALLOCATE(atoms, forces, msgbuffer)
-    END PROGRAM IPI_DRIVER
+        ENDIF
+    ENDDO
+    IF (nat > 0) DEALLOCATE(atoms, forces, msgbuffer)
+END PROGRAM IPI_DRIVER
