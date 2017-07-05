26dd201c2fc052ec4312a087c1d68b9cf4cda8d3
Jan Kessler <jakessle@t-online.de>
2017-07-05 15:12:42 +0200

    Added pilot command to compile ipi_client

--- pilot-HswfQMC.sh
+++ pilot-HswfQMC.sh
@@ -34,18 +34,19 @@ do
 								"
 			echo "      --- Build and set the HswfQMC code ---"
 			echo "set_path - set the PATH shell variable in order to be able to use the pilot script, the HswfQMC_exe executalbe, and the helpers"
-         echo "install_markuspline - Download and compile the markuspline library (https://github.com/francesco086/markuspline)"
+			echo "install_markuspline - Download and compile the markuspline library (https://github.com/francesco086/markuspline)"
 			echo "install_lapack - Download and compile the lapack library (recommended) "
 			echo "set_makefile - Set the Makefile automatically"
 			echo "build - Compile (make) the HswfQMC source code in order to have the HswfQMC_exe executable"
 			echo "rebuild - Recompile the code from scratch"
+			echo "make_ipic - Compile (make) the i-pi client, which connects HswfQMC as a force generator to i-pi."
 			echo "git_pull - Update the code to the latest version on GitHub (it might require a rebuild)"
 			
 			echo ""
 			echo "      --- Use HswfQMC ---"
 			echo "set_dir - Make the current folder a working folder (with all necessary input files and folders)"
-         echo "generate_orbitals - Compute the DFT orbitals corresponding to the given input file dati_fisici.d and dati_DFT.d using Quantum Espresso (it requires pw.x and iotk from Quantum Espresso)"
-         echo "find_SR_minimum - Find the minimum of a SR minimization, by looking at the file ottimizzazione/SR_energies.d"
+			echo "generate_orbitals - Compute the DFT orbitals corresponding to the given input file dati_fisici.d and dati_DFT.d using Quantum Espresso (it requires pw.x and iotk from Quantum Espresso)"
+			echo "find_SR_minimum - Find the minimum of a SR minimization, by looking at the file ottimizzazione/SR_energies.d"
 			echo "clean - Clean all old data from previous simulations"
 			echo "wash - Clean all old data from previous simulations but the optimized wf and lattice positions"
 			
@@ -179,6 +180,15 @@ do
 			fi
 			exit
 			;;
+		make_ipic)
+			cd ${pilot_PATH}
+			echo "Build the i-pi client executable ipi_client.x"
+			cd ipi_client
+			make
+                        mv ipi_client.x ../
+			cd $CURRENT_PATH
+			exit
+			;;
 		commit)
 			echo "Commit and push on github."
 			echo "Are you sure? [y/n] "
@@ -287,7 +297,7 @@ export PATH=$PATH
 _pilot-HswfQMC.sh()
 {
     local cur=\${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=( \$(compgen -W \"git_pull set_path install_markuspline install_lapack set_makefile build rebuild set_dir generate_orbitals find_SR_minimum clean wash commit\" -- \$cur) )
+    COMPREPLY=( \$(compgen -W \"git_pull set_path install_markuspline install_lapack set_makefile build rebuild make_ipic set_dir generate_orbitals find_SR_minimum clean wash commit\" -- \$cur) )
 }
 complete -F _pilot-HswfQMC.sh pilot-HswfQMC.sh" >> ~/.${FILE_TO_SET}
 			exit
